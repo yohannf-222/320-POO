400cb6070f6b7381b6086ea39ea0fe0f55d8b262
yohannf-222 <yohann.femminis@gmail.com>
2026-09-11 11:00:55 +0200

    feat(RNndValuesHelpers): ajouter un helper pour les valeurs aleatoires

---
+++ personnel/Exercices/Drones/Helpers/RndValueHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones.Helpers
+{
+    internal static class RndValueHelpers
+    {
+        private static Random alea= new Random();   
+
+    }
+}
