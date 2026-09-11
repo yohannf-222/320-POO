db1daedba96c4401a18776b37e321976126a645a
yohannf-222 <yohann.femminis@gmail.com>
2026-09-11 09:29:26 +0200

    chore(personnel): Ajouter le zip Drone fourni
    Dossier Drone = base pour les exercices futurs

---
+++ personnel/Exercices/Drones/Model/Drone.cs
@@ -0,0 +1,59 @@
+﻿using Drones.Helpers;
+using Drones.Properties;
+
+namespace Drones
+{
+    // Cette partie de la classe Drone définit ce qu'est un drone par un modèle numérique
+    public partial class Drone
+    {
+        public int charge;                            // La charge actuelle de la batterie
+        public string name;                           // Un nom
+        public int x;                                 // Position en X depuis la gauche de l'espace aérien
+        public int y;                                 // Position en Y depuis le haut de l'espace aérien
+
+        // Constructeur
+        public Drone(int x, int y, string name)
+        {
+            Random alea = new Random();
+            this.x = x;
+            this.y = y;
+            this.name = name;
+            charge = alea.Next(1000); // La charge initiale de la batterie est choisie aléatoirement
+        }
+
+        #region ================ Modelisation du drone et de son comportement ================
+
+        // Cette méthode calcule le nouvel état dans lequel le drone se trouve après
+        // que 'interval' millisecondes se sont écoulées
+        public void Update(int interval)
+        {
+            if (charge <= 0) return;                     // S'il n'a plus de charge, il ne peut plus bouger
+            Random alea = new Random();
+            x += 2;                                    // Il s'est déplacé de 2 pixels vers la droite
+            y += alea.Next(-2, 3);                     // Il s'est déplacé d'une valeur aléatoire vers le haut ou le bas
+            charge--;                                  // Il a dépensé de l'énergie
+        }
+
+        #endregion
+
+        #region  ================ Rendu graphique  ================
+
+        private const int SIZE = 50;
+        private Pen droneBrush = new Pen(new SolidBrush(Color.Purple), 3);
+
+        // De manière graphique
+        public void Render(BufferedGraphics drawingSpace)
+        {
+            drawingSpace.Graphics.DrawImage(charge > 0 ? Resources.drone : Resources.boom, x-Drone.SIZE/2, y - Drone.SIZE / 2, Drone.SIZE, Drone.SIZE);
+            drawingSpace.Graphics.DrawString($"{this}", TextHelpers.drawFont, TextHelpers.writingBrush, x + 5, y - 25);
+        }
+
+        // De manière textuelle
+        public override string ToString()
+        {
+            return $"{name} ({((int)((double)charge / 1000 * 100)).ToString()}%)";
+        }
+        #endregion
+
+    }
+}
