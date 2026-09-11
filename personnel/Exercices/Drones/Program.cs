562b4028b40105f2a2105dc3f845885baa506fdd
yohannf-222 <yohann.femminis@gmail.com>
2026-09-11 11:41:24 +0200

    fix(main) : adapter la position initiale du drone à Config

--- personnel/Exercices/Drones/Program.cs
+++ personnel/Exercices/Drones/Program.cs
@@ -1,3 +1,5 @@
+using Drones.Helpers;
+
 namespace Drones
 {
     internal static class Program
@@ -14,8 +16,8 @@ namespace Drones
 
             // Création de la flotte de drones
             List<Drone> fleet= new List<Drone>();
-            fleet.Add(new Drone(AirSpace.WIDTH / 2, AirSpace.HEIGHT / 2, "Joe"));
-            
+            fleet.Add(new Drone(Config.AIRSPACE_WIDTH / 2, Config.AIRSPACE_HEIGHT / 2, "Joe"));
+
             // Démarrage
             Application.Run(new AirSpace(fleet));
         }
