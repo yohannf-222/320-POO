8dbeba4e04b73286fadf0c5968082cb24918485b
yohannf-222 <yohann.femminis@gmail.com>
2026-09-11 11:02:08 +0200

    feat(Config): ajouter un Heper pour la config
    attributs:
    MAX_LOAD
    AIRSPACE_WIDTH
    AIRSPACE_HEIGHT

---
+++ personnel/Exercices/Drones/Helpers/Config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones.Helpers
+{
+    internal class Config
+    {
+        private static int MAX_LOAD = 1000;
+        private static int AIRSPACE_WIDTH = 1200;
+        private static int AIRSPACE_HEIGHT = 600;
+
+    }
+}
--- personnel/Exercices/Drones/Program.cs
+++ personnel/Exercices/Drones/Program.cs
@@ -15,7 +15,7 @@ namespace Drones
             // Création de la flotte de drones
             List<Drone> fleet= new List<Drone>();
             fleet.Add(new Drone(AirSpace.WIDTH / 2, AirSpace.HEIGHT / 2, "Joe"));
-
+            
             // Démarrage
             Application.Run(new AirSpace(fleet));
         }
