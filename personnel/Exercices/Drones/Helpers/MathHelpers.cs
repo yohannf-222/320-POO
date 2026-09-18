3ff42b84c537a622030342a2969401691c1abb15
yohannf-222 <yohann.femminis@gmail.com>
2026-09-18 11:21:28 +0200

    Creation d'un helper pour la distance entre 2 points.

---
+++ personnel/Exercices/Drones/Helpers/MathHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones.Helpers
+{
+    internal class MathHelpers
+    {
+        public static double CalculateDistance(double x1, double y1, double x2, double y2)
+        {
+            double deltaX = x2 - x1;
+            double deltaY = y2 - y1;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+    }
+}
