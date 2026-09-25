2fa5549023b93a1a39d9b87d109099f42f3a98b5
yohannf-222 <yohann.femminis@gmail.com>
2026-09-25 11:40:32 +0200

    feat(pizzeria) : ajouter la ressource pour la pizzeria.

--- personnel/Exercices/Drones/Properties/Resources.Designer.cs
+++ personnel/Exercices/Drones/Properties/Resources.Designer.cs
@@ -89,5 +89,15 @@ namespace Drones.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Pizzeria {
+            get {
+                object obj = ResourceManager.GetObject("Pizzeria", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
