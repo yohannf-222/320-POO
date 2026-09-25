729469d8fd913f525749e8d5905f882ee665c935
yohannf-222 <yohann.femminis@gmail.com>
2026-09-25 11:40:06 +0200

    refactor(Program): générer la flotte en utilisant la méthode.

--- personnel/Exercices/Drones/Program.cs
+++ personnel/Exercices/Drones/Program.cs
@@ -16,12 +16,11 @@ namespace Drones
             ApplicationConfiguration.Initialize();
 
             // Création de la flotte de drones
-            List<Drone> fleet = new List<Drone>();
-            fleet.Add(new Drone(Config.AIRSPACE_WIDTH - 200, Config.AIRSPACE_HEIGHT - 100, "Joe"));
-            fleet.Add(new Drone(Config.AIRSPACE_WIDTH - 600, Config.AIRSPACE_HEIGHT - 200, "Peter"));
-            fleet.Add(new Drone(Config.AIRSPACE_WIDTH - 800, Config.AIRSPACE_HEIGHT - 300, "Cleveland"));
+            List<Drone> fleet = new List<Drone>(Drone.GenerateFleet(3));
 
             Charger charger = new Charger(Config.AIRSPACE_WIDTH / 2, Config.AIRSPACE_HEIGHT / 2);
+
+
 
             // Démarrage
             Application.Run(new AirSpace(fleet, charger));
