fdc3ac1d6d24850296e478121032509c9b258208
yohannf-222 <yohann.femminis@gmail.com>
2026-09-25 11:13:28 +0200

    feat(ExerciceTests) : Un exercice simple sur les méthodes de tests.

---
+++ personnel/Exercices/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class MyMath
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ personnel/Exercices/TestProject1/Test1.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp1;
+
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSum()
+        {
+            //Arrange
+            int x = 10;
+            int y = 5;
+            int z = -10;
+
+            //Act
+            int res = MyMath.Sum(x, y);
+            int res1 = MyMath.Sum(y, z);
+
+            //Assert
+            Assert.AreEqual(15, res);
+            Assert.AreEqual(-5, res1);
+        }
+    }
+}
