0db4722ca1e137d85f4394708b4016c0b1eaef3a
yohannf-222 <yohann.femminis@gmail.com>
2026-09-25 12:02:59 +0200

    fix(program) : générer 5 pizzerias au lieu de 4

--- personnel/Exercices/Drones/Program.cs
+++ personnel/Exercices/Drones/Program.cs
@@ -20,7 +20,7 @@ namespace Drones
 
             Charger charger = new Charger(Config.AIRSPACE_WIDTH / 2, Config.AIRSPACE_HEIGHT / 2);
 
-            List<Pizzeria> pizzerias = new List<Pizzeria>(Pizzeria.GeneratePizzerias(4));
+            List<Pizzeria> pizzerias = new List<Pizzeria>(Pizzeria.GeneratePizzerias(5));
 
             // Démarrage
             Application.Run(new AirSpace(fleet, charger, pizzerias));
